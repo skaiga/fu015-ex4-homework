194f2623b620329ba90a8b59045e3ad530e89139
海賀奏志 <kaiga@wlan-napt-006.u-aizu.ac.jp>
2022-07-14 16:53:57 +0900

    HoT: initial revision

---
+++ ex4.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+int main(){
+    int judge;
+    int count=0;
+
+    srand((unsigned int)time(NULL));
+
+    printf("Tossing a coin...\n");
+
+    for(int i=0 ; i<3 ; i++){
+        judge = rand();
+        if(judge%2 == 0){
+            printf("Round %d: Heads\n",i+1);
+            count++;
+        }
+        else {
+            printf("Round %d: Tails\n",i+1);
+        }
+    }
+
+    printf("Heads: %d, Tails: %d\n",count,3-count);
+}
